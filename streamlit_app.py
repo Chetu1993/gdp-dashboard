2742c20772f62b15f518f7be926bda0b17895018
Chetu1993 <103425365+Chetu1993@users.noreply.github.com>
2025-06-03 08:56:55 +0000

    newchange

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,11 +3,16 @@ import pandas as pd
 import math
 from pathlib import Path
 
+
+
 # Set the title and favicon that appear in the Browser's tab bar.
 st.set_page_config(
     page_title='GDP dashboard',
     page_icon=':earth_americas:', # This is an emoji shortcode. Could be a URL too.
 )
+
+st.title("my new app")
+st.title("my new development app")
 
 # -----------------------------------------------------------------------------
 # Declare some useful functions.
